407cb558da007e3e287ac07d681ffbc77a9971d0
shu Lin <bbqlinshu@gmail.com>
2021-11-26 17:17:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (drivers, lookUpVal) => {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === lookUpVal.toLowerCase();
+    })
+}
+
+const fuzzyMatch = (drivers, firstTwoLetter) => {
+    return drivers.filter((driver)=>{
+        return driver.toLowerCase().slice(0,2) === firstTwoLetter.toLowerCase();
+    })
+}
+
+const matchName = (drivers, lookUpVal) => {
+    return drivers.filter((driver) => {
+        return driver.name === lookUpVal;
+    })
+}
